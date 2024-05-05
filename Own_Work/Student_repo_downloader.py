bc8a2a9d6ef281e7455dbb7e22c4c66964f0ca6c
Aleksander V Eriksen <70018977+AleksanderVEriksen@users.noreply.github.com>
2024-05-05 20:09:27 +0200

    Update Student_repo_downloader.py

--- Own_Work/Student_repo_downloader.py
+++ Own_Work/Student_repo_downloader.py
@@ -6,21 +6,22 @@ Created on Fri Feb 16 15:06:50 2024
 """
 # In[0]: 
 import git # pip install gitpython
-
 text_file = open("students.txt", "r").read().split("\n")
+
+working_dir = "text"
+github_link = "text"
+
 for student in text_file:   
     try:
-        git.Repo.clone_from(f'https://github.com/HPC2024/labs-{student}.git', f'C:\\Users\\eriks\\OneDrive - Østfold University College\\HPC2024_TA\\Students\\labs-{student}')
+        git.Repo.clone_from(f'{github_link}labs-{student}.git', f'{working_dir}labs-{student}')
     except git.exc.GitCommandError as expection:
             print(f"Error occured for {student} as {expection}")
     SET = True
     if SET:
-        current_lab = 5  # Lab to be graded
+        current_lab = 15  # Lab to be graded
     import os
     import shutil
     for x in range(1, current_lab):
-        dirpath = os.path.join(f'C:\\Users\\eriks\\OneDrive - Østfold University College\\HPC2024_TA\\Students\\labs-{student}', f'lab{x}')
+        dirpath = os.path.join(f'C:{working_dir}labs-{student}', f'lab{x}')
         if os.path.exists(dirpath) and os.path.isdir(dirpath):
             shutil.rmtree(dirpath)
-# In[1]:
-
